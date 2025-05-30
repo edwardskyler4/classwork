f2596256f1b0cb09d2d96737ae93affbb3e4b35b
Kyler Edwards <edwardskyler4@gmail.com>
2025-05-29 20:07:18 -0600

    test: Add tests for new water flow functions

--- cse111/test_water_flow.py
+++ cse111/test_water_flow.py
@@ -1,7 +1,7 @@
 from pytest import approx
 import pytest
 
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
+from water_flow import *
 
 def test_water_column_height():
     assert water_column_height(0, 0) == 0
@@ -22,5 +22,27 @@ def test_pressure_loss_from_pipe():
     assert pressure_loss_from_pipe(0.048692, 200, 0.018, 1.65) == approx(-100.462, 0.001)
     assert pressure_loss_from_pipe(0.28687, 1000, 0.013, 1.65) == approx(-61.576, 0.001)
     assert pressure_loss_from_pipe(0.28687, 1800.75, 0.013, 1.65) == approx(-110.884, 0.001)
+
+def test_pressure_lost_from_fittings1():
+    assert pressure_loss_from_fittings(0, 3) == approx(0, 0.001)
+    assert pressure_loss_from_fittings(1.65, 0) == approx(0, 0.001)
+def test_pressure_lost_from_fittings3():
+    assert pressure_loss_from_fittings(1.65, 2) == approx(-0.109, 0.001)
+def test_pressure_lost_from_fittings4():
+    assert pressure_loss_from_fittings(1.75, 2) == approx(-0.122, 0.001)
+def test_pressure_lost_from_fittings5():
+    assert pressure_loss_from_fittings(1.75, 5) == approx(-0.306, 0.001)
+
+def test_reynolds_number():
+    assert reynolds_number(0.048692, 0) == approx(0, 1)
+    assert reynolds_number(0.048692, 1.65) == approx(80069, 1)
+    assert reynolds_number(0.048692, 1.75) == approx(84922, 1)
+    assert reynolds_number(0.28687, 1.65) == approx(471729, 1)
+    assert reynolds_number(0.28687, 1.75) == approx(500318, 1)
+
+def test_pressure_loss_from_pipe_reduction():
+    assert pressure_loss_from_pipe_reduction(0.28687, 0, 1, 0.048692) == approx(0, 0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(-163.744, 0.001)
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(-184.182, 0.001)
 
 pytest.main(["-v", "--tb=line", "-rN", __file__])
